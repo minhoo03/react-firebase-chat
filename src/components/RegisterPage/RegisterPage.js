f7979fe87e858951d802eaf521d8f8d4523d7ca9
minhoo03 <minhoo0333@gmail.com>
2021-01-24 04:07:03 +0900

    register page css

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,18 +1,45 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function RegisterPage() {
     return (
-        <div>
+        <div className="auth_wrapper">
+            <div className="auth_title">
+                <h3>Register</h3>
+            </div>
             <form>
-                <label>Example</label>
-                <input name="example" defaultValue="test" />
-                <label>ExampleRequired</label>
+                <label>Email</label>
                 <input
-                    name="exampleRequired"
+                    name="email"
+                    type="email"
+                    //ref={register({ required: true, maxLength: 10 })}
+                />
+                {/* {errors.exampleRequired && <p>This field is required</p>} */}
+
+                <label>Name</label>
+                <input
+                    name="name"
+                    //ref={register({ required: true, maxLength: 10 })}
+                />
+                {/* {errors.exampleRequired && <p>This field is required</p>} */}
+
+                <label>Password</label>
+                <input
+                    name="password"
+                    type="password"
+                    //ref={register({ required: true, maxLength: 10 })}
+                />
+                {/* {errors.exampleRequired && <p>This field is required</p>} */}
+
+                <label>Password Confirm</label>
+                <input
+                    name="password_confirm"
+                    type="password"
                     //ref={register({ required: true, maxLength: 10 })}
                 />
                 {/* {errors.exampleRequired && <p>This field is required</p>} */}
                 <input type="submit" />
+                <Link style={{color: '#adadad', textDecoration: 'none'}} to="Login">이미 아이디가 있다면...</Link>
             </form>
         </div>
     )

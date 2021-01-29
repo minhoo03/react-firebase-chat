417bd4aa2f2a3f0fb2971e112c78128264a3177f
minhoo03 <minhoo0333@gmail.com>
2021-01-29 11:13:43 +0900

    message DB에 변동 있을 때 -> 해당 경로(chatRoom)의 msg를 state에 담음 => component에 state 전달

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -3,8 +3,52 @@ import MessageHeader from './MessageHeader'
 import Message from './Message'
 import MessageForm from './MessageForm'
 
+import { connect } from 'react-redux'
+import firebase from '../../../firebase'
+
 export class MainPanel extends Component {
+
+    state = {
+        messages: [],
+        messageRef: firebase.database().ref("messages"),
+        messagesLoading: true
+    }
+
+    // 각 방의 채팅 구분
+    componentDidMount() {
+        const {chatRoom} = this.props
+        if(chatRoom) {
+            this.addMessagesListeners(chatRoom.id)
+        }
+    }
+
+    // DB 변동 O => state에 msg 담음
+    addMessagesListeners = (chatRoomId) => {
+        let messagesArr = []
+        this.state.messageRef.child(chatRoomId).on("child_added", snapShot => {
+            messagesArr.push(snapShot.val())
+            this.setState({ 
+                messages: messagesArr,
+                messagesLoading: false
+            })
+        })  
+    }
+
+    // msg state를 Message component에 전달
+    renderMessages = (messages) => {
+        messages.length > 0 &&
+        messages.map(msg => (
+            <Message
+                key={msg.timestamp}
+                message={msg}
+                user={this.props.user}
+            />
+        ))
+    }
+
     render() {
+        const{ messages } = this.state
+
         return (
             <div style={{padding: '2rem 2rem 0 2rem'}}>
                 <MessageHeader />
@@ -18,7 +62,8 @@ export class MainPanel extends Component {
                     marginBottom:'1rem',
                     overflowY:'auto'
                 }}>
-                    {/* <Message /> */}
+                    {/* msg 렌더링 */}
+                    {this.renderMessages(messages)}
                 </div>
                 <MessageForm />
             </div>
@@ -26,4 +71,10 @@ export class MainPanel extends Component {
     }
 }
 
-export default MainPanel
+const mapStateToProps = state => {
+    return {
+        user: state.user.currenUser,
+        chatRoom: state.chatRoom.currentChatRoom
+    }
+}
+export default connect(mapStateToProps)(MainPanel)

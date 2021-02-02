61eacdfe81197e217eca24223d086dca77be0575
minhoo03 <minhoo0333@gmail.com>
2021-02-02 15:49:13 +0900

    Listener를 이용해 User DB의 유저 정보를 가져옴 -> Redux의 currentUser를 확인 후 자신을 제외한 모든 유저 정보를 가져옴 -> 해당 유저의 DB값 (이미지, 이름)에 uid와 status를 추가해 Array에 담음 -> user State에 Array를 전송

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react'
+import firebase from '../../../firebase'
 import { AiOutlineSmile } from 'react-icons/ai'
+import { connect } from 'react-redux'
 
 export class DirectMessages extends Component {
 
+    state = {
+        usersRef: firebase.database().ref("users")
+    }
+
+    componentDidMount() {
+        if(this.props.user){
+            this.addUserListeners(this.props.user.uid)
+        }
+    }
+
+    addUserListeners = (currentUserId) => {
+        const {usersRef} = this.state
+        let usersArray = []
+        usersRef.on("child_added", DataSnapshot => {
+            if(currentUserId !== DataSnapshot.key) { // 로그인 된 내 아이디를 제외한 DB 유저들의 key값
+                let user = DataSnapshot.val() // DataSnapshot: 유저 정보를 user에 담음(현재는 이름과 이미지만)
+                user["uid"] = DataSnapshot.key // 유저의 모든 정보를 가진 key를 이용해 uid도 추가해줌
+                user["status"] = "offline"
+
+                usersArray.push(user) // 수정한 user정보를 userArray에 담고 state로 전송
+                this.setState({users: usersArray})
+            }
+        })
+    }
+
     renderDirectMessages = () => {
-        
+
     }
 
     render() {
+        console.log('users', this.state.users)
         return (
             <div>
                 <span style={{display:'flex', alignItems:'center'}}>
@@ -22,4 +50,9 @@ export class DirectMessages extends Component {
     }
 }
 
-export default DirectMessages
+const mapStateToProps = state => {
+    return {
+        user: state.user.currentUser
+    }
+}
+export default connect(mapStateToProps)(DirectMessages)

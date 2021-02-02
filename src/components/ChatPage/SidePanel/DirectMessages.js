c5874feec5c828bccf830ea026730c0a0ff07116
minhoo03 <minhoo0333@gmail.com>
2021-02-02 15:29:32 +0900

    DM pannel UI

--- src/components/ChatPage/SidePanel/DirectMessages.js
+++ src/components/ChatPage/SidePanel/DirectMessages.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { AiOutlineSmile } from 'react-icons/ai'
 
-function DirectMessages() {
-    return (
-        <div>
-            DirectMessages   
-        </div>
-    )
+export class DirectMessages extends Component {
+
+    renderDirectMessages = () => {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <span style={{display:'flex', alignItems:'center'}}>
+                    <AiOutlineSmile style={{marginRight: 3}} /> Direct Messages(1)
+                </span>
+
+                <ul style={{listStyleType: 'none', padding: 0}}>
+                    {this.renderDirectMessages()}
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default DirectMessages

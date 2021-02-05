a764d418df2d6591f13bf7c8804288f4b381a1ee
minhoo03 <minhoo0333@gmail.com>
2021-02-05 13:23:51 +0900

    componentDidmount -> AddChatRoomsListener { addNotificationListener(렌더링 채팅방_id) } => handleNotification(렌더링 채팅방, Redux의 채팅방, 현재 알림, 채팅방의 모든 value) => *findIndex를 통해 조건에 맞는 값이 없다면 -1을 반환해서.. 새로운 알림이 있는 방과 없는 방 구분 -> 만약 새로운 알림이 있는 방일시 -> (그 방에 내가 있다, 내가 없다) -> 전체 메시지 - 새로운 메시지 => state에 삽입 후 렌더링

--- src/components/ChatPage/SidePanel/ChatRooms.js
+++ src/components/ChatPage/SidePanel/ChatRooms.js
@@ -7,6 +7,7 @@ import { AiOutlinePlus } from 'react-icons/ai'
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
+import Badge from 'react-bootstrap/Badge'
 
 import { connect } from 'react-redux'
 import firebase from '../../../firebase'
@@ -19,10 +20,12 @@ export class ChatRooms extends Component {
         show: false,
         name: "",
         description: "",
+        messageRef:firebase.database().ref("messages"),
         chatRoomsRef: firebase.database().ref("chatRooms"),
         chatRooms: [],
         firstLoad: true,
-        activeChatRoomId: ""
+        activeChatRoomId: "", // 선택되었나
+        notifications: [], // 채팅룸 알림
     } 
 
     // useEffect
@@ -30,10 +33,69 @@ export class ChatRooms extends Component {
         this.AddChatRoomsListeners()
     }
 
-    // AddChat...Listeners의 eventListener "child_added"를 종료
-    // componentWillUnmount() {
-    //     this.state.chatRooms.off()
-    // }
+    // const handleClose = () => setShow(false);
+    handleClose = () => this.setState({show: false})
+    handleShow = () => this.setState({show: true})
+
+    // chatroom 데이터 실시간 받기
+    AddChatRoomsListeners = () => {
+        let chatRoomsArray = []
+
+        // .on : DB에 child가 추가되는지 실시간 확인 중
+        this.state.chatRoomsRef.on("child_added", DataSnapshot => {
+            chatRoomsArray.push(DataSnapshot.val())
+            // console.log('chatRoomsArray',chatRoomsArray)
+            this.setState({chatRooms: chatRoomsArray}, () => this.setFirstChatRoom()) // 확인된 DB child를 setState
+            this.addNotifivationListener(DataSnapshot.key) // chatRoom.id
+        })
+    }
+
+    addNotifivationListener = (chatRoomId) => { // child_added에 의해 1~n번째 방까지 전부 Listener 실행
+        this.state.messageRef.child(chatRoomId).on("value", DataSnapshot => {
+            if(this.props.chatRoom) {
+                this.handleNotification(
+                    chatRoomId, // 렌더링 된 n번지 방
+                    this.props.chatRoom.id, // 현재 내 방
+                    this.state.notifications,
+                    DataSnapshot
+                )
+            }
+        })
+    }
+
+    handleNotification = (chatRoomId, currentChatRoomId, notifications, DataSnapshot) => {
+
+        let lastTotal = 0
+
+        // 이미 notifications state 안에 알림 정보가 들어있는 채팅방과 그렇지 않은 채팅방을 나눠주기 (새로 만든 방)
+        let index = notifications.findIndex(notifivation => notifivation.id === chatRoomId)
+        
+        // notifications state안에 해당 채팅방 알림 정보가 없을 때
+        if(index === -1) {
+            notifications.push({
+                id: chatRoomId,
+                total: DataSnapshot.numChildren(),
+                lastKnownTotal: DataSnapshot.numChildren(),
+                count: 0
+            })
+        } else { // 이미 해당 채팅방 알림 정보가 있을 때
+            // 상대방이 채팅 보내는 채팅룸에 내가 있지 않을 때
+            if(chatRoomId !== currentChatRoomId) {
+                // 현재까지 유저가 확인한 총 메시지 갯수
+                lastTotal = notifications[index].lastKnownTotal
+
+                // count 알림으로 보여줄 숫자 구하기
+                // 현재 총 메시지 개수 - 이전에 확인한 메시지 개수 > 0
+                if(DataSnapshot.numChildren() - lastTotal > 0) {
+                    notifications[index].count = DataSnapshot.numChildren() - lastTotal
+                }
+            }
+            // total property에 현재 전체 메시지 개수 넣어주기
+            notifications[index].total = DataSnapshot.numChildren()
+        }
+        // 목표는 방 하나 하나의 맞는 알림 정보를 notifivations state에 넣어주기
+        this.setState({ notifications })
+    }
 
     // 임의로 첫 번째 ROOM이 redux에 들어가지게
     setFirstChatRoom = () => {
@@ -47,44 +109,10 @@ export class ChatRooms extends Component {
         this.setState({firstChatRoom: false})
     }
 
-    // chatroom 데이터 실시간 받기
-    AddChatRoomsListeners = () => {
-        let chatRoomsArray = []
-
-        // .on : DB에 child가 추가되는지 실시간 확인 중
-        this.state.chatRoomsRef.on("child_added", DataSnapshot => {
-            chatRoomsArray.push(DataSnapshot.val())
-            // console.log('chatRoomsArray',chatRoomsArray)
-            this.setState({chatRooms: chatRoomsArray}, () => this.setFirstChatRoom()) // 확인된 DB child를 setState
-        })
-    }
-
-    // const handleClose = () => setShow(false);
-    handleClose = () => this.setState({show: false})
-    handleShow = () => this.setState({show: true})
-
-    // 유효성 체크 (state 있는지 확인)
-    isFormVaild = (name, description) => name && description 
-
-    // room 정보를 redux에 넣기
-    changeChatRoom = (room) => {
-        this.props.dispatch(setCurrentChatRoom(room))
-        this.props.dispatch(setPrivateChatRoom(false))
-        this.setState({activeChatRoomId: room.id})
-    }
-
-    // state: chatroom에 값이 있을 경우 map을 이용해 렌더링
-    renderChatRooms = (chatRooms) => 
-        chatRooms.length > 0 && chatRooms.map(room => (
-            <li 
-                key={room.id}
-                style={{backgroundColor: room.id === this.state.activeChatRoomId && "#ffffff45"}}
-                onClick={() => this.changeChatRoom(room)} // li 클릭시 redux 저장
-            >
-                # {room.name}
-            </li>
-        ))
-    
+    // AddChat...Listeners의 eventListener "child_added"를 종료
+    // componentWillUnmount() {
+    //     this.state.chatRooms.off()
+    // }
 
     // form submit
     handleSubmit = (e) => {
@@ -122,6 +150,45 @@ export class ChatRooms extends Component {
         } catch (error) {
             alert(error)
         }
+    }
+
+    // 유효성 체크 (state 있는지 확인)
+    isFormVaild = (name, description) => name && description 
+
+    // room 정보를 redux에 넣기
+    changeChatRoom = (room) => {
+        this.props.dispatch(setCurrentChatRoom(room))
+        this.props.dispatch(setPrivateChatRoom(false))
+        this.setState({activeChatRoomId: room.id})
+    }
+
+    // state: chatroom에 값이 있을 경우 map을 이용해 렌더링
+    renderChatRooms = (chatRooms) => 
+    chatRooms.length > 0 && chatRooms.map(room => (
+        <li 
+            key={room.id}
+            style={{backgroundColor: room.id === this.state.activeChatRoomId && "#ffffff45"}}
+            onClick={() => this.changeChatRoom(room)} // li 클릭시 redux 저장
+        >
+            # {room.name}
+            {/* 읽지 않은 알림 */}
+            <Badge variant="danger" style={{float:'right', marginTop:'4px'}}>
+                {this.getNotificationCount(room)}
+            </Badge>
+        </li>
+    ))
+
+    getNotificationCount = (room) => {
+        // 해당 채팅방 count 수 구하기
+        let count = 0
+
+        this.state.notifications.forEach(notifications => {
+            if(notifications.id === room.id){
+                count = notifications.count
+            }
+        })
+
+        if(count > 0) return count
     }
 
     render() {
@@ -179,7 +246,8 @@ export class ChatRooms extends Component {
 // redux state를 props로 바꿔 사용하겠다 / class component라 HOOK 사용 불가
 const mapStateToProps = state => {
     return {
-        user: state.user.currentUser
+        user: state.user.currentUser,
+        chatRoom: state.chatRoom.currentChatRoom
     }
 }
 export default connect(mapStateToProps)(ChatRooms)

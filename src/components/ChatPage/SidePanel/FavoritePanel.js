e3baf8d643d39a3d7fbfc7d41fb6825aaddbea32
minhoo03 <minhoo0333@gmail.com>
2021-02-07 19:26:33 +0900

    Build favorite UI!.. componentDidMount => addListners => DB의 favorite 확인 후 state로 -> *DB의 값이 true/false에 따라 state 변경 => state 렌더링 -> onClick 이벤트로 chatRoom 정보 보낸 후 redux에 보낸다 -> 화면에 현재 redux의 chatRoom을 확인

--- src/components/ChatPage/SidePanel/FavoritePanel.js
+++ src/components/ChatPage/SidePanel/FavoritePanel.js
@@ -1,11 +1,92 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {AiOutlineSmile} from 'react-icons/ai'
+import firebase from '../../../firebase'
+import {connect} from 'react-redux'
 
-function FavoritePanel() {
-    return (
-        <div>
-            FavoritePanel
-        </div>
-    )
+import { setCurrentChatRoom, setPrivateChatRoom } from '../../../Redux/actions/chatRoom_action'
+
+export class FavoritePanel extends Component {
+
+    state = {
+        usersRef : firebase.database().ref('users'),
+        favoritedChatRoom: [],
+        activeChatRoomId: ''
+    }
+
+
+    componentDidMount() {
+        if(this.props.user) {
+            this.addListeners(this.props.user.uid) // 현재 로그인 된 유저
+        }
+    }
+
+
+    addListeners = (userId) => {
+        const { usersRef } = this.state
+
+        usersRef
+            .child(userId)
+            .child('favorited')
+            .on('child_added', DataSnapshot => {
+                const favoritedChatRoom = { id: DataSnapshot.key, ...DataSnapshot.val()}
+
+                this.setState({
+                    favoritedChatRoom: [...this.state.favoritedChatRoom, favoritedChatRoom] // 기존 state favoriteCR, const favoriteCR
+                })
+            })
+
+        usersRef
+            .child(userId)
+            .child('favorited')
+            .on('child_removed', DataSnapshot => {
+                const chatRoomToRemove = { id: DataSnapshot.key, ...DataSnapshot.val()}
+                
+                const filteredChatRooms = this.state.favoritedChatRoom.filter(chatRoom => { // 기존 favoriteCR을 하나 하나 chatRoom에 담음
+                    return chatRoom.id !== chatRoomToRemove.id // 이제 없는 CR을 걸러서 return
+                })
+                this.setState({favoritedChatRoom: filteredChatRooms})
+
+        })
+    }
+
+
+    changeChatRoom = (chatRoom) => { // onClick으로 chatRoom 정보가 넘어온다
+        this.props.dispatch(setCurrentChatRoom(chatRoom)) // 현재 내가 있는 방을 Redux로
+        this.props.dispatch(setPrivateChatRoom(false))
+        this.setState({activeChatRoomId: chatRoom.id})
+    }
+
+
+    renderFavotiredChatRoom = (favoritedChatRoom) => {
+        return favoritedChatRoom.length > 0 && favoritedChatRoom.map(chatRoom => (
+            <li key={chatRoom.id} onClick={() => this.changeChatRoom(chatRoom)} 
+                style={{backgroundColor:chatRoom.id === this.state.activeChatRoomId && '#ffffff45'}}
+            >
+                # {chatRoom.name}
+            </li>
+        ))
+    }
+
+
+    render() {
+        const { favoritedChatRoom } = this.state
+        return (
+            <div>
+                <span style={{display:'flex', alignItems:'center'}}>
+                    <AiOutlineSmile style={{marginRight: '3px'}} /> FAVORITE (1)
+                </span>
+
+                <ul style={{listStyleType: 'none', padding: '0'}}>
+                    {this.renderFavotiredChatRoom(favoritedChatRoom)}
+                </ul>
+            </div>
+        )
+    }
 }
 
-export default FavoritePanel
+const mapStateToProps = state => {
+    return {
+        user: state.user.currentUser
+    }
+}
+export default connect(mapStateToProps)(FavoritePanel)
